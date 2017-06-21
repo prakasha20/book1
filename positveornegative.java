19a2cf08bca04e21d7159a6fe47900ba20765d2d
prakasha20 <prakash.a0497@gmail.com>
2017-06-21 14:57:36 +0530

    Update and rename gfdzfh.java to positveornegative.java

---
+++ positveornegative.java
@@ -0,0 +1,21 @@
+
+
+import java.io.*;
+import java.util.*;
+class positveornegative
+{
+    public static void main(String[] args)
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        if(a==0)
+            System.out.println("ZERO");
+        if(a<0)
+            System.out.println("NEGATIVE");
+        if(a>0)
+            System.out.println("POSITIVE");
+      
+    }
+    
+    
+}
